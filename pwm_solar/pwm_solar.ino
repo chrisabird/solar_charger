238a097ee164275864fd66394f78ba099599053c
chris <chris@christopherbird.co.uk>
2017-01-16 21:38:30 +0000

    Implement simple float charge

--- pwm_solar/pwm_solar.ino
+++ pwm_solar/pwm_solar.ino
@@ -2,54 +2,55 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 
+#define voltageDivider 0.203389830508
+#define supplyVoltage 5.0
+#define chargeVoltage 13.5
+#define chargePwmPin 3
+#define solarVoltagePin A0
+#define batteryVoltagePin A1
+
 Adafruit_PCD8544 display = Adafruit_PCD8544(12, 11, 10, 9, 8);
+uint8_t duty = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-
   display.begin();
-
   display.setContrast(50);
-  
-  display.display(); // show splashscreen
-  delay(2000);
-  display.clearDisplay();   // clears the screen and buffer
 
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(3, OUTPUT);
-  analogWrite(3, 32);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  pinMode(solarVoltagePin, INPUT);
+  pinMode(batteryVoltagePin, INPUT);
+  pinMode(chargePwmPin, OUTPUT);
+
+  analogWrite(chargePwmPin, 0);
 }
 
 double readVoltage(uint8_t voltagePin) {
-  double reading = 0;
-  reading = analogRead(voltagePin);
-  delay(100);
-  reading = analogRead(voltagePin);
-  reading += analogRead(voltagePin);
-  reading += analogRead(voltagePin);
-  reading += analogRead(voltagePin);
-  reading += analogRead(voltagePin);
-  reading += analogRead(voltagePin);
-  reading += analogRead(voltagePin);
-  reading += analogRead(voltagePin);
-  reading += analogRead(voltagePin);
-  reading += analogRead(voltagePin);
-  return ((5.0 * (reading / 10)) / 1024) /  (1200.0 / (4700.0 + 1200.0));
-
-  
+  analogRead(voltagePin);
+  delay(10);
+  return ((analogRead(voltagePin) * supplyVoltage) / 1024) / voltageDivider;
 }
 
 void loop() {
+  double solarVoltage = readVoltage(solarVoltagePin);
+  double batteryVoltage = readVoltage(batteryVoltagePin);
+
+  if (batteryVoltage < chargeVoltage && duty < 255t) {
+    duty++;
+  }
+  if (batteryVoltage > chargeVoltage && duty > 0) {
+    duty--;
+  }
+  analogWrite(chargePwmPin, duty);
+
   display.clearDisplay();
   display.print("SOLAR ");
-  display.println(readVoltage(A0));
+  display.println(solarVoltage);
   display.print("BAT ");
-  display.println(readVoltage(A1));
+  display.println(batteryVoltage);
   display.print("DUTY ");
-  display.println(128);
+  display.println(duty);
   display.display();
-
-  
-
 }
